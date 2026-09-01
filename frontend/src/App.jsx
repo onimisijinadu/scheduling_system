75d6a10e455f322be6bb60dcd8a856172b5f4a9c
onimisijinadu <jinaduabdulkareem340@gmail.com>
2026-09-02 00:22:17 +0100

    fixed frontend .git file

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero"></div>
+        <div></div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  );
+}
+
+export default App;
